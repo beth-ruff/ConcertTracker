3b32d4a42a8533d2f1a5854163c847550d3419cd
Beth Ruff <beth.ruff2@gmail.com>
2020-07-14 18:42:43 -0700

    set up rack-cors gem

--- concert-tracker-backend/config/application.rb
+++ concert-tracker-backend/config/application.rb
@@ -8,7 +8,7 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_mailbox/engine"
+require "action_mailbox/engine"z
 require "action_text/engine"
 require "action_view/railtie"
 require "action_cable/engine"
@@ -21,6 +21,13 @@ Bundler.require(*Rails.groups)
 
 module ConcertTrackerBackend
   class Application < Rails::Application
+    
+    config.middleware.insert_before 0, Rack::Cors do 
+      allow do 
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post]
+      end 
+    end 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
